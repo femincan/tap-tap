85db5d214e251c75694e39cec1aab752e833934a
Furkan Emin Can <femincanx@gmail.com>
2023-05-20 19:40:07 +0300

    feat(styling): add base styling, theme, and font

--- src/GlobalStyle.tsx
+++ src/GlobalStyle.tsx
@@ -1,6 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyle = createGlobalStyle`
+  :root {
+    --space-cadet: #2b2d42;
+    --cool-gray: #8d99ae;
+    --antiflash-white: #edf2f4;
+    --red-pantone: #ef233c;
+    --fire-engine-red: #d90429;
+  }
+  
   * {
     padding: 0;
     margin: 0;
@@ -11,6 +19,13 @@ const GlobalStyle = createGlobalStyle`
     width: 100vw;
     max-width: 100%;
     height: 100vh;
+    font-family: 'Open Sans', sans-serif;
+    font-size: 62.5%;
+  }
+
+  body {
+    font-size: 1.6rem;
+    color: var(--antiflash-white)
   }
 
   #app {
