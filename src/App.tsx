29f17f30538ee5f19437e047d1de31d6aa39c56f
Furkan Emin Can <femincanx@gmail.com>
2023-05-22 00:23:26 +0300

    feat: add all components into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-const App = () => <div>App</div>;
+import { Container, Logo, DrumMachine, Footer } from './components';
+
+const App = () => (
+  <Container>
+    <Logo />
+    <DrumMachine />
+    <Footer />
+  </Container>
+);
 
 export { App };
