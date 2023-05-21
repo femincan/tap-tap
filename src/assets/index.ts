c45f03e85e6698c79659e2b7b0c5ebfc4443f340
Furkan Emin Can <femincanx@gmail.com>
2023-05-21 13:42:29 +0300

    feat(assets): add audio files

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+const key1 = new URL('./Heater-1.mp3', import.meta.url);
+const key2 = new URL('./Heater-2.mp3', import.meta.url);
+const key3 = new URL('./Heater-3.mp3', import.meta.url);
+const key4 = new URL('./Heater-4.mp3', import.meta.url);
+const key5 = new URL('./Clap.mp3', import.meta.url);
+const key6 = new URL('./Open-HH.mp3', import.meta.url);
+const key7 = new URL("./Kick-n'-hat.mp3", import.meta.url);
+const key8 = new URL('./Kick.mp3', import.meta.url);
+const key9 = new URL('./Closed-HH.mp3', import.meta.url);
+
+export { key1, key2, key3, key4, key5, key6, key7, key8, key9 };
